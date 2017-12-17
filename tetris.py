eaef92a2cf4679fe0d0fb2490a83bb39cbd7e26e
Ahn Ji <lyzqm@naver.com>
2017-12-18 01:52:28 +0900

    UI 개선, DQN알고리즘 사용
    UI 개선 
    - test, score UI 추가
    
    DQN 알고리즘 사용
    - CNN으로 84*84*1의 입력을 사용
    - 약 25만개의 변수 사용
    - 50시간 학습

--- tetris.py
+++ tetris.py
@@ -7,33 +7,44 @@ PhotoImage = ImageTk.PhotoImage
 np.random.seed(1)
 UNIT = 30  # 픽셀 수
 HEIGHT = 20  # 그리드 세로
-WIDTH = 10   # 그리드 가로
-MID = UNIT*4 #블록 시작 점
-dy = [UNIT, 0, 0]       #action에 해당하는 y좌표 plus값
-dx = [0, UNIT, -UNIT]   #action에 해당하는 y좌표 plus값
+WIDTH = 8   # 그리드 가로
+MID = (WIDTH / 2 - 1) * UNIT # 블록 시작 점
+dy = [UNIT, 0, 0]       # action에 해당하는 y좌표 plus값
+dx = [0, UNIT, -UNIT]   # action에 해당하는 y좌표 plus값
+PLUS_SCORE = 10.0
+basic_counter_str = 'test : '
+basic_score_str = 'score : '
+
+
 class Env(tk.Tk):
     def __init__(self):
         super(Env, self).__init__()
         self.score = 0.0
         self.score_weight = []
+        self.counter = 0
         for i in range(HEIGHT):
             if i <= 2:
                 self.score_weight.append(0.0)
             else:
-                self.score_weight.append((i-2)*(i-2)*0.01382976)
-
+                self.score_weight.append((i-2)*(i-2)*0.00082976)
         self.action_space = ['d', 'l', 'r']
         self.action_size = len(self.action_space)
         self.color = ["red","blue","green","yellow","purple"]
         self.block_kind = len(self.color)
 
         self.curr_block = np.random.randint(self.block_kind)
-        self.canvas = self._build_canvas()
-        self.map = self._build_map()
-        self.counter = 0
-    def _build_map(self):
-        map = [[0]*WIDTH for i in range(HEIGHT)]
-        return map
+        self.canvas, self.counter_board, self.score_board = self._build_canvas()
+        self.map = [[0]*WIDTH for _ in range(HEIGHT)]
+
+    def _get_curr_block_pos(self):
+        ret = []
+        for n in range(4):
+            s = (self.canvas.coords(self.block[n]))
+            y = int(s[1] / UNIT)
+            x = int(s[0] / UNIT)
+            ret.append([y,x])
+        return ret
+
     def _clear_map(self):
         for n in range(HEIGHT):
             for m in range(WIDTH):
@@ -70,12 +81,22 @@ class Env(tk.Tk):
     def _build_canvas(self):
         canvas = tk.Canvas(self, bg='black',
                            height=HEIGHT * UNIT,
-                           width=WIDTH * UNIT)
-        '''
+                           width=(WIDTH + 5)* UNIT)
+        counter_board = canvas.create_text((WIDTH + 3) * UNIT, int(HEIGHT / 4 * UNIT),
+                           fill = "white",
+                           font = "Times 10 bold",
+                           text = basic_counter_str + str(int(self.counter)))
+
+        score_board = canvas.create_text((WIDTH + 3)*UNIT, int(HEIGHT/2*UNIT),
+                           fill = "white",
+                           font = "Times 10 bold",
+                           text = basic_score_str + str(int(self.score)))
+
         # 그리드 생성
-        for c in range(0, WIDTH * UNIT, UNIT):  # 0~400 by 80
+        for c in range(0, (WIDTH + 1)* UNIT, UNIT):  # 0~400 by 80
             x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
-            canvas.create_line(x0, y0, x1, y1)
+            canvas.create_line(x0, y0, x1, y1, fill = 'white')
+        '''
         for r in range(0, HEIGHT * UNIT, UNIT):  # 0~400 by 80
             x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
             canvas.create_line(x0, y0, x1, y1)
@@ -97,8 +118,8 @@ class Env(tk.Tk):
                                         tag="rect")
         self.block = [rect1,rect2,rect3,rect4]
         canvas.pack()
+        return canvas,counter_board,score_board
 
-        return canvas
     def make_block(self):
         x, y = MID, 0
         pos = [[[x, y], [x + UNIT, y], [x, y + UNIT], [x + UNIT, y + UNIT]],
@@ -109,19 +130,29 @@ class Env(tk.Tk):
         return pos[self.curr_block]
 
     def reset(self):
-        time.sleep(0.3)
+        self.score = 0.0
+        self.counter += 1
+        self.canvas.itemconfigure(self.counter_board,
+                                  text=basic_counter_str + str(int(self.counter)))
+        self.canvas.itemconfigure(self.score_board,
+                                  text=basic_score_str + str(int(self.score)))
         self.update()
         self.canvas.delete("rect")
         self._clear_map()
         self._add_canvas()
-        self.score = 0.0
+
     def step(self, action):
         self.render()
-        if not self.move(action):
+        reward = self.move(action)
+        if reward >= 0.00000:
             # make new block!
             self.curr_block = np.random.randint(self.block_kind)
             self._add_canvas()
         self.canvas.tag_raise(self.block)
+        if reward < 0.00000:
+            return 0.0
+        else:
+            return reward
 
     def possible_to_move(self, action):
         for n in range(len(self.block)):
@@ -129,14 +160,14 @@ class Env(tk.Tk):
             y = s[1] + dy[action]
             x = s[0] + dx[action]
 
-            #범위밖 - stay
-            if x >= (WIDTH) * UNIT or x < 0:
+            # 범위밖 - stay
+            if x >= WIDTH * UNIT or x < 0:
                 return 1
             ny = int(y/UNIT)
             nx = int(x/UNIT)
 
-            #마지막줄 - add canvas
-            if y >= (HEIGHT) * UNIT:
+            # 마지막줄 - add canvas
+            if y >= HEIGHT * UNIT:
                 return 2
             if self.map[ny][nx] == 1:
                 if action == 0:
@@ -144,20 +175,22 @@ class Env(tk.Tk):
                 else:
                     return 1
 
-        #이동가능함 - move
+        # 이동가능함 - move
         return 3
 
     def is_map_horizon(self):
         for n in range(HEIGHT - 1, 0, -1):
             cnt = 0
             for m in range(WIDTH):
-                if self.map[n][m] == 1:
-                    cnt += 1
+                if self.map[n][m] != 1:
+                    break
+                cnt += 1
             if cnt == WIDTH:
                 return n
         return -1
 
     def move(self, action):
+        ret = 0.0
         base_action = np.array([0, 0])
         flag = self.possible_to_move(action)
 
@@ -168,22 +201,28 @@ class Env(tk.Tk):
                 y = int(s[1] / UNIT)
                 x = int(s[0] / UNIT)
                 self.score += self.score_weight[y]
+                ret += self.score_weight[y]
                 self.map[y][x] = 1
 
             # 한줄이 꽉차있으면 비워주고 점수를 더해줌
+            break_cnt = 0
             while True:
                 y = self.is_map_horizon()
                 if y == -1:
                     break
                 self._erase_down_canvas(y)
                 self._move_all_canvas_down(y)
-                self.score += 100
+                break_cnt += 1
                 for m in range(WIDTH):
-                    for n in range(HEIGHT -1 , 1, -1):
+                    for n in range(y , 2, -1):
                         self.map[n][m] = self.map[n-1][m]
-            return False
+            self.score += PLUS_SCORE * break_cnt
+            ret += PLUS_SCORE * break_cnt
+            self.canvas.itemconfigure(self.score_board,
+                                      text = basic_score_str + str(int(self.score)))
+            return ret
 
-        #move
+        # move
         elif flag == 3:
             base_action[1] += dy[action]
             base_action[0] += dx[action]
@@ -191,7 +230,7 @@ class Env(tk.Tk):
                 self.canvas.move(self.block[n], base_action[0], base_action[1])
 
         self.canvas.coords(self.block)
-        return True
+        return -1.0
 
     def is_game_end(self):
         for n in range(3):
